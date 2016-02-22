b28721e6b1123dc03f510c253e8a1fe3f6f70d78
Malachi Gray <graymd.cfi@gmail.com>
2016-02-21 21:56:52 -0500

    16x16 square grid

---
+++ js/javascripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  for (var i = 0; i< 256; i++) {
+    $newSquare = $('<div class="square"></div>');
+    $('#wrapper').append($newSquare);
+    console.log('wtf?')
+  }
+});
+
