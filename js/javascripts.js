a5c76826c44c2983ff510d5536c19092c33a5a64
Malachi Gray <graymd.cfi@gmail.com>
2016-02-21 22:19:19 -0500

    add clear sketchpad button - working on select number of squares

--- js/javascripts.js
+++ js/javascripts.js
@@ -2,7 +2,17 @@ $(document).ready(function() {
   for (var i = 0; i< 256; i++) {
     $newSquare = $('<div class="square"></div>');
     $('#wrapper').append($newSquare);
-    console.log('wtf?')
   }
+
+  $('.square').mouseover(function(){
+    $(this).addClass("newColor");
+  });
+
+  $('button').click(function(){
+    console.log('wtf');
+    $('.square').removeClass("newColor");
+    var numSquares=prompt('how many total squares would you like?')
+      console.log(numSquares);
+  });
 });
 
